33445657e31b12f6cc367252d811ed66295116ec
xanddybr <xanddybr@gmail.com>
2025-03-12 02:59:50 -0300

    inserted mytimestamp

---
+++ public/myTimeStamp.js
@@ -0,0 +1,13 @@
+function getMyTimeZone(){
+    const now = new Date().toLocaleString("br-BR", { timeZone: "America/Sao_Paulo" });
+    const date = new Date(now)
+    const hours = String(date.getHours()).padStart(2, '0')
+    const minutes = String(date.getMinutes()).padStart(2, '0');
+    const seconds = String(date.getSeconds()).padStart(2, '0');
+    const day = String(date.getDate()).padStart(2, '0');
+    const month = String(date.getMonth() + 1).padStart(2, '0');
+    const year = date.getFullYear();
+    return console.log(`${hours}:${minutes}:${seconds} ${day}/${month}/${year}`)
+  }
+
+module.exports = getMyTimeZone
